5a23a069315e09ad2c5b120ccc427e0c3eca7d4a
anshulimentus <anshul.dhakad@imentus.com>
2025-11-05 19:14:29 +0530

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from 'config/database.config';
+import { typeOrmConfig } from '../config/database.config';
 import { CustomerModule } from './customer/customer.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
